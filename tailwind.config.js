a91a666ce21676433983fcef3f50ea14dfbcc98e
Jaredd <jaredd@Jared-Dahlkes-Macbook.local>
2022-10-18 06:31:28 -0700

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				cyan: colors.cyan
+			}
+		}
+	},
+	plugins: [
+		// ...
+		require('@tailwindcss/forms')
+	]
+}
